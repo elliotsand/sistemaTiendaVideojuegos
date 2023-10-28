e163d5c348cfa2baa44b2e1398794a090cadba9f
emilio <elliotsand2>
2023-10-27 21:40:07 -0500

    preodcuto

--- ProyectoFinal/src/proyectotienda/clases/Producto.java
+++ ProyectoFinal/src/proyectotienda/clases/Producto.java
@@ -1,15 +1,17 @@
 package proyectotienda.clases;
 
 public class Producto {
-	private static int codigoProductoCounter = 3001;
-    private int codigoProducto;
-    private String nombre;
-    private String descripcion;
-    private double precio;
+	private static int codigoProductoCounter = 3000;
+	private static int correlativoCounter = 1;
+	private int codigoProducto;
+	private String nombre;
+	private String descripcion;
+	private double precio;
 
  
     public Producto( String nombre, String descripcion, double precio ) {
-    	this.codigoProducto = codigoProductoCounter++;
+    	this.codigoProducto = generarCodigoProducto();
+        generarCorrelativo();
         this.nombre = nombre;
         this.descripcion = descripcion;
         this.precio = precio;
@@ -42,7 +44,9 @@ public class Producto {
     public double getPrecio() {
         return precio;
     }
-
+    
+   
+    
     public void setPrecio(double precio) {
         this.precio = precio;
     }
@@ -52,6 +56,13 @@ public class Producto {
         
         double precioConIGV = precio * 1.18;
         return precioConIGV;
+    }
+    private static int generarCorrelativo() {
+        return correlativoCounter++;
+    }
+    
+    private static int generarCodigoProducto() {
+        return codigoProductoCounter++;
     }
     
     public static void establecerCorrelativo(int valor) {
